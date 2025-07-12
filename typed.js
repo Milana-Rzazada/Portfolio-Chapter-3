5aeef2d8cc8c3610826f16d0330b2a796529503b
Milana-Rzazade <71l1c0h@code.edu.az>
2025-07-12 15:36:15 -0400

    Add typing animation with Typed.js.

---
+++ typed.js
@@ -0,0 +1,27 @@
+const words = new Typed('#words', {
+      strings: [
+        "Hello, I'm Milana. I'm a Front-End developer and Web Developer.",
+        "I have over 3 years of work experience in this field. I have studied in this field, developed projects, and gained experience both in Azerbaijan and Canada.",
+        "Welcome to my Portfolio website about myself and the work I do! You can see some of the projects I have completed in the Projects section.",
+        "Enjoy browsing my portfolio!",
+        
+      ],
+      typeSpeed: 50,
+      backSpeed: 30,
+      backDelay: 1500,
+      loop: true
+    });
+
+    const thanks = new Typed('#thanks-typed', {
+      strings: [
+        "I hope you liked my portfolio.",
+        "If you're reading this post, it means you've read my portfolio all the way through.",
+        "Thank you for your attention and for the time you have given me!",
+        "SEE YOU!",
+        
+      ],
+      typeSpeed: 50,
+      backSpeed: 30,
+      backDelay: 1500,
+      loop: true
+    });
